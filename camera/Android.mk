1bc9cf34e5b6ae6cdd96ccc1fc968d5d6ff2442f
Tony Layher <layhertony@gmail.com>
2014-11-16 03:27:31 -0500

    msm8974-common: Fixup CameraWrapper for L
      -Also clean up code and get rid of warnings.
       Clean up from Arne Coucheron works on other samsung wrappers.
    
      -Requires device tree to include a CameraParametersExtra.h
       for the specific device.
    
    Change-Id: Icab1b9ec17710861f735a05dcdd87dac932c63c2

--- camera/Android.mk
+++ camera/Android.mk
@@ -12,8 +12,11 @@ LOCAL_C_INCLUDES += \
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.msm8974
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
